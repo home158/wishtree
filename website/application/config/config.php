289238f9beb8ec612a3fe0aabfaa7e84592e534e
home158 <home158@gmail.com>
2015-06-30 21:07:01 +0800

    GoAzure

--- website/application/config/config.php
+++ website/application/config/config.php
@@ -70,6 +70,7 @@ $config['url_suffix'] = '';
 |
 */
 $config['language']	= 'zh-tw';
+$config['available_lang'] = array("en-us", "zh-tw");
 
 /*
 |--------------------------------------------------------------------------
