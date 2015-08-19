b4ee195acbef7e039ac3f035da70ced0651a601f
home158 <home158@gmail.com>
2015-08-19 23:41:15 +0800

    guid

--- website/application/views/site/chat/room.php
+++ website/application/views/site/chat/room.php
@@ -15,7 +15,7 @@
   <script>
 $(function() {
     var connected = false;
-    var socket = io.connect('http://wishpool.azurewebsites.net/chat/room/', {
+    var socket = io.connect('http://wishpool.azurewebsites.net', {
         query: 'guid={GUID}'
     });
     socket.on("GUID_duplicated",function(){
