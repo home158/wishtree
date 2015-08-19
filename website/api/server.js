b07668402e3ccfe4589b82a69abea2754eccad3c
home158 <home158@gmail.com>
2015-08-19 23:01:05 +0800

    query

--- website/api/server.js
+++ website/api/server.js
@@ -16,19 +16,15 @@ app.use(express.static(__dirname + '/public'));
 
 // Chatroom
 
-// usernames which are currently connected to the chat
-var usernames = {};
-var numUsers = 0;
+
 /*
     socket.emit 只有自己
     socket.broadcast.emit 除了自己
     io.emit 包含自己
 */
-io.on('connection', function (socket) {
+io.sockets.on('connection', function (socket) {
     socket.on('join_chatroom', function (data) {
-        io.emit('send_message', {
-            numUsers: 5
-        });
+
         socket.emit('login_welcome', {
             numUsers: 5
         });
@@ -38,34 +34,12 @@ io.on('connection', function (socket) {
         });
 
     });
-    var addedUser = false;
 
     // when the client emits 'new message', this listens and executes
-    socket.on('new message', function (data) {
-        // we tell the client to execute 'new message'
-        socket.broadcast.emit('new message', {
-            username: socket.username,
-            message: data
-        });
+    socket.on('send_message', function (data) {
+        io.to(data.targetGUID).emit('send_message', 'hello');
     });
 
-    // when the client emits 'add user', this listens and executes
-    socket.on('add user', function (username) {
-        // we store the username in the socket session for this client
-        socket.username = username;
-        // add the client's username to the global list
-        usernames[username] = username;
-        ++numUsers;
-        addedUser = true;
-        socket.emit('login', {
-            numUsers: numUsers
-        });
-        // echo globally (all clients) that a person has connected
-        socket.broadcast.emit('user joined', {
-            username: socket.username,
-            numUsers: numUsers
-        });
-    });
 
 
 
--- website/application/views/site/chat/room.php
+++ website/application/views/site/chat/room.php
@@ -5,31 +5,20 @@
   <title>Socket.IO Chat Example</title>
 </head>
 <body>
-  <ul class="pages">
-    <li class="chat page">
-      <div class="chatArea">
-        <ul class="messages"></ul>
-      </div>
-      <input class="inputMessage" placeholder="Type here..."/>
-    </li>
-    <li class="login page">
-      <div class="form">
-        <h3 class="title">What's your nickname?</h3>
-        <input class="usernameInput" type="text" maxlength="14" />
-      </div>
-    </li>
-  </ul>
+  
+    <input id="targetGUID" type="text" value="" />
+    <button id="private">Send</button>
+  
 
   <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
   <script src="/socket.io/socket.io.js"></script>
   <script>
 $(function() {
     var connected = false;
-    var socket = io();
-
-    socket.on('send_message', function (data) {
-        console.log('包含自己');
+    var socket = io.connect('http://wishpool.azurewebsites.net/chat/room', {
+        query: 'userId={GUID}'
     });
+
     
     socket.on('client_joined', function (data) {
         console.log('除了自己');
@@ -43,6 +32,15 @@ $(function() {
         UserGUID: '{GUID}', 
         Role: '{Role}',
         Nickname: '{Nickname}'
+    });
+    socket.emit("send_message", function(data){
+        console.log(data)
+    });
+    $('#private').on('click',function(){
+        socket.emit("send_message", {
+            targetGUID: $('#targetGUID').val(),
+            message: 'Hello private message from {Nickname}'
+        });
     });
 });
 
