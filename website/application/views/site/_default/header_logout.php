c423ab8ab7a653e0784f896b3e45fde111f2848f
home158 <home158@gmail.com>
2015-09-04 19:05:03 +0800

    fix

--- website/application/views/site/_default/header_logout.php
+++ website/application/views/site/_default/header_logout.php
@@ -1,7 +1,7 @@
 <div id="header">
     <div class="hd">
         <h1 class="">
-        <a href="http://www.asugardating.com/">Wish Tree</a></h1>
+        <a href="#">Wish Tree</a></h1>
         <ul class="header-link">
       	
             <li><a href="">升級 VIP</a></li>
